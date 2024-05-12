c0ffaa6e790780d4c66c642141c1c9fa436ab7e9
karanyadavv <karanyadav729@gmail.com>
2024-05-12 19:33:18 +0530

    sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           disableTransitionOnChange
           storageKey="lotion-theme"
           >
+            <Toaster position="bottom-center"/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
